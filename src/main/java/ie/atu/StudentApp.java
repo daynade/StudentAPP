ccc35f34fca3f25fe2cea65c473338012deefb60
David <G00420905@atu.ie>
2025-10-08 17:03:11 +0100

    Added prompt for name, email and password

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -7,8 +7,11 @@ public class StudentApp {
     {
         Scanner sc = new Scanner(System.in);
         ArrayList<Student> students = new ArrayList<>();
+        System.out.println("Enter Student Name: ");
+        String name = sc.nextLine();
         System.out.println("Enter your student email");
         String email = sc.nextLine();
-
+        System.out.println("Enter your student password");
+        String password = sc.nextLine();
     }
 }
