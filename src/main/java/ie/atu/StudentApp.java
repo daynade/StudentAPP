3d7743ad17ac8e496ee097ebf830df9467292418
David <G00420905@atu.ie>
2025-10-08 16:52:17 +0100

    Created ArrayList and user prompts

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,7 +1,14 @@
 package ie.atu;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class StudentApp {
-    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Student> students = new ArrayList<>();
+        System.out.println("Enter your student email");
+        String email = sc.nextLine();
 
+    }
 }
